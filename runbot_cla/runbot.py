34cc9fcd6981c8ac58b9b1e7858015cc7216bab6
Christophe Monniez <moc@odoo.com>
2018-03-07 10:08:32 +0100

    fix traceback when cla not encoded in utf-8

--- runbot_cla/runbot.py
+++ runbot_cla/runbot.py
@@ -5,7 +5,6 @@ import logging
 import re
 
 from odoo import models
-from odoo.tools import ustr
 
 _logger = logging.getLogger(__name__)
 
@@ -16,21 +15,25 @@ class runbot_build(models.Model):
     def _job_05_check_cla(self, build, lock_path, log_path):
         cla_glob = glob.glob(build._path("doc/cla/*/*.md"))
         if cla_glob:
-            cla = ''.join(open(f).read() for f in cla_glob)
-            cla = ustr(cla.lower())
+            description = "%s Odoo CLA signature check" % build.author
             mo = re.search('[^ <@]+@[^ @>]+', build.author_email or '')
             state = "failure"
             if mo:
                 email = mo.group(0).lower()
                 if re.match('.*@(odoo|openerp|tinyerp)\.com$', email):
                     state = "success"
-                if cla.find(email) != -1:
-                    state = "success"
-                _logger.info('CLA build:%s email:%s result:%s', build.dest, email, state)
+                else:
+                    try:
+                        cla = ''.join(open(f).read() for f in cla_glob)
+                        if cla.lower().find(email) != -1:
+                            state = "success"
+                    except UnicodeDecodeError:
+                        description = 'Invalid CLA encoding (must be utf-8)'
+                    _logger.info('CLA build:%s email:%s result:%s', build.dest, email, state)
             status = {
                 "state": state,
                 "target_url": "https://www.odoo.com/sign-cla",
-                "description": "%s Odoo CLA signature check" % build.author,
+                "description": description,
                 "context": "legal/cla"
             }
             build._log('check_cla', 'CLA %s' % state)
